113fa9c4352d36d5354a1ab5fb593fe61eef7171
Arpita K Sau <arpitakaran@gmail.com>
2020-06-09 00:30:09 -0500

    fixed-business-hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,50 +4,47 @@ var $businessHours = $(".business-hours");
 var $saveButton = $(".btn");
 var $container = $(".container-fluid");
 var currentTime = getCurrentTime();
-console.log("Current Time: " + currentTime.format('LT'));
+
 //creating array of $businessHours
-//var businessHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 var businessHours = [];
+
 var events = JSON.parse(localStorage.getItem('events')) || {};
-  console.log(events);   
+
 // this function is to make sure javascript starts  after HTML is loaded
 $(document).ready(function() {
   populateBusinessHours();
   populateRows();
   getTodaysDate();
 });  
+
 function populateBusinessHours() {
   var currentDOW = moment().isoWeekday();
   if (currentDOW <= 5) {
-    var startTime = moment('09:00 am', "HH:mm a");
-    var endTime = moment('05:00 pm', "HH:mm p");
-    amIBetween = currentTime.isBetween(startTime , endTime);
-    if (amIBetween == true){
-      console.log("Need to do some work");
-    }
-    else{
-      console.log("Relax now... had a long day");
-    }
     for(var i = 0; i < 9; i++) {
-      var updatedTime = startTime.add(moment.duration(i, 'hours')).format('LT');
-      console.log("Working Time: " + i + '-' + updatedTime);
-      businessHours.push(updatedTime);
+      var initialTime = moment('09:00 am', "HH:mm a"); 
+      var incrementalTime = initialTime.add(moment.duration(i, 'hours'));
+      businessHours.push(incrementalTime);
+    }
+
+    for(var i = 0; i < businessHours.length; i++) {
+      console.log(businessHours[i].format('LT'));
+      amIBetween = currentTime.isBetween(businessHours[i], businessHours[i+1]);
+      pastTime = currentTime.isAfter(businessHours[i+1]);
+      
+      if (pastTime === true) {
+        console.log("I am grey");
+        //$(".event-text").get(i).attr("class", "past");
+      } else {
+        if (amIBetween === true) {
+          console.log("I am red");
+          //$(".event-text").get(i).attr("class", "present");
+        }else {
+          console.log("I am green");
+          //$(".event-text").get(i).attr("class", "future");
+        }
+      }
     }
   }
-  // if (currentDOW ==6) {
-  //   var startTime = moment('09:00 am', "HH:mm a");
-  //   var endTime = moment('06:00 pm', "HH:mm a");
-  //   amIBetween = currentTime.isBetween(startTime , endTime);
-  //   if (amIBetween == true){
-  //     document.write('We are currently open. We close today at 6:00PM');
-  //   }
-  //   else{
-  //     document.write('We are currently closed. We will be open again Monday at 8:00AM');
-  //   }
-  // }
-  // if (currentDOW ==7) {
-  //   document.write('We are currently closed. We will be open again on Monday at 8:00AM');
-  // }
 }
 //Function to get date using moment.js
 //Refer https://momentjs.com/docs/ search for LLLL and http://zetcode.com/javascript/momentjs/
@@ -66,8 +63,7 @@ function populateRows (){
     var hoursDiv = $("<div>");
     var inputDiv = $("<div>");
     var buttonDiv =$("<div>");
-    console.log("Business hours: " + businessHours[i]);
-    var hours = $("<p>" +businessHours[i] + "<p>");
+    var hours = $("<p>" +businessHours[i].format('LT') + "<p>");
     var button = $("<button>");
     var input = $("<input>");
     $container.append(rowDiv);
@@ -90,7 +86,6 @@ function populateRows (){
   };
 };
 //add event listener to save button
-//for ( var j = 0; j < businessHours.length; j++ ) {
   $('body').on("click", '.action', function(){
     var buttonIndex = $(this).data('index');
     var inputText = $(".event-text").get(buttonIndex).value;
@@ -98,7 +93,6 @@ function populateRows (){
         return;
     }
     events['input' + buttonIndex] = inputText;
-  // events.push(inputText);
   console.log(events);
   localStorage.setItem("events", JSON.stringify(events));
 });
